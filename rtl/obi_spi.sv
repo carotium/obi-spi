ff75685c610eb3e592417a2bb345e72c30758c16
carotium <coolboy9987@gmail.com>
2026-06-04 10:24:26 +0000

    Removed parameter BASE_ADDR. Obi xbar takes care of that. Moved Addr_width, data_width and spi_data length to localparams as they are always 32, 32, 8 as aforementioned. Removed txbufferempty and rxbuffernonempty as they are redundant. Combined obi states writing and reading into one obi state transfer.

--- rtl/obi_spi.sv
+++ rtl/obi_spi.sv
@@ -1,10 +1,6 @@
 module obi_spi #(
-  parameter int unsigned BASE_ADDR                = 0,
-  parameter int unsigned ADDR_WIDTH               = 32,
-  parameter int unsigned DATA_WIDTH               = 32,
   parameter int unsigned NUM_SLAVES               = 4,
-  parameter int unsigned SCLK_COUNTER_RESET_VALUE = 0,
-  parameter int unsigned SPI_DATA_LENGTH          = 8
+  parameter int unsigned SCLK_COUNTER_RESET_VALUE = 0
 ) (
   input logic clk_i,
   input logic rstn_i,
@@ -13,16 +9,16 @@ module obi_spi #(
   //  A channel
   input   logic                     obi_areq_i,
   output  logic                     obi_agnt_o,
-  input   logic [ADDR_WIDTH-1:0]    obi_aaddr_i,
-  input   logic [DATA_WIDTH-1:0]    obi_awdata_i,
+  input   logic [AddrWidth-1:0]    obi_aaddr_i,
+  input   logic [DataWidth-1:0]    obi_awdata_i,
 
   input   logic                     obi_awe_i,
-  input   logic [DATA_WIDTH/8-1:0]  obi_abe_i,
+  input   logic [DataWidth/8-1:0]  obi_abe_i,
 
   //  R channel
   output  logic                     obi_rvalid_o,
   input   logic                     obi_rready_i,
-  output  logic [DATA_WIDTH-1:0]    obi_rdata_o,
+  output  logic [DataWidth-1:0]    obi_rdata_o,
   output  logic                     obi_rerr_o,   // Response Error - TODO
 
   // SPI master
@@ -38,27 +34,27 @@ module obi_spi #(
   **********                 LOCALPARAM                **********
   **************************************************************/
 
+  localparam int AddrWidth = 32;
+  localparam int DataWidth = 32;
+  localparam int SpiDataLength = 8;
+
   // Register Address Offsets
-  localparam int TxDataRegAddrOffset        = BASE_ADDR + 0;
-  localparam int RxDataRegAddrOffset        = BASE_ADDR + 4;
-  localparam int SpiDivClkRegAddrOffset     = BASE_ADDR + 8;
-  localparam int SsRegAddrOffset            = BASE_ADDR + 12;
-  localparam int CtrlRegAddrOffset          = BASE_ADDR + 16;
+  localparam int TxDataRegAddrOffset        = 0;
+  localparam int RxDataRegAddrOffset        = 4;
+  localparam int SpiDivClkRegAddrOffset     = 8;
+  localparam int SsRegAddrOffset            = 12;
+  localparam int CtrlRegAddrOffset          = 16;
 
   // Control Register Bits
   localparam int CtrlStartWritingBit         = 0;
   localparam int CtrlStartReadingBit         = 1;
   localparam int CtrlBusyBit                 = 2;
-  localparam int CtrlTxBufferEmptyBit        = 3;
-  localparam int CtrlRxBufferNonEmptyBit     = 4;
-  localparam int CtrlCompleteBit             = 5;
+  localparam int CtrlCompleteBit             = 3;
 
   // Control Register Bit Masks
   localparam int CtrlStartWritingBitMask     = 1 << CtrlStartWritingBit;
   localparam int CtrlStartReadingBitMask     = 1 << CtrlStartReadingBit;
   localparam int CtrlBusyBitMask             = 1 << CtrlBusyBit;
-  localparam int CtrlTxBufferEmptyBitMask    = 1 << CtrlTxBufferEmptyBit;
-  localparam int CtrlRxBufferNonEmptyBitMask = 1 << CtrlRxBufferNonEmptyBit;
   localparam int CtrlCompleteBitMask         = 1 << CtrlCompleteBit;
 
   // SPI Serial Clock Max Value
@@ -79,24 +75,16 @@ module obi_spi #(
   // OBI states
   typedef enum reg [1:0] {
     eOBI_IDLE,       // Waiting for instructions
-    eOBI_READING,    // OBI read transfer
-    eOBI_WRITING     // OBI write transfer
+    eOBI_TRANSFER    // OBI read/write transfer
   } obi_state_t;
 
   /**************************************************************
   **********                DEFINITIONS                **********
   **************************************************************/
 
-  // TX data register
-  logic [SPI_DATA_LENGTH-1:0] tx_data_reg;
-
-  // RX data register
-  logic [SPI_DATA_LENGTH-1:0] rx_data_reg;
-
-  // SPI Division clock register
-  logic [DATA_WIDTH-1 : 0] spi_div_clk_reg;
-
-  // SS Register
+  logic [SpiDataLength-1:0] tx_data_reg;
+  logic [SpiDataLength-1:0] rx_data_reg;
+  logic [DataWidth-1 : 0] spi_div_clk_reg;
   logic [NUM_SLAVES-1 : 0] ss_reg;
 
   // Control Register Bits
@@ -106,17 +94,12 @@ module obi_spi #(
   logic ctrl_start_reading_bit; 
   //  2 bit: Busy - currently sending or reading on SPI
   logic ctrl_busy_bit;
-  //  3 bit: TX buffer empty - clears on OBI write to TX register
-  logic ctrl_tx_buffer_empty_bit;
-  //  4 bit: RX buffer non-empty - clears on OBI read from RX register
-  logic ctrl_rx_buffer_non_empty_bit;
-  //  5 bit: Complete (TX or RX) - "Writable" - sets after SPI transaction completes, waits for clear from CPU - IRQ for CPU
+  //  3 bit: Complete (TX or RX) - "Writable" - sets after SPI transaction completes, waits for clear from CPU - IRQ for CPU
   logic ctrl_complete_bit;
 
   logic ctrl_complete_bit_next, ctrl_complete_bit_write;
 
-  // Control Register Register Value
-  logic [5:0] ctrl_reg_value;
+  logic [3:0] ctrl_reg_value;
 
   // OBI
   //  Obi Address Channel Accepted Transaction
@@ -126,16 +109,17 @@ module obi_spi #(
   //  Obi Address Channel Valid Write Transaction
   logic obi_a_write_valid;
 
+  logic obi_started_transfer;
   logic obi_started_reading, obi_started_writing, obi_done;
 
   obi_state_t obi_state, obi_state_next;
 
-  logic [DATA_WIDTH-1 : 0] obi_read_value;
+  logic [DataWidth-1 : 0] obi_read_value;
 
   // SPI
   logic [3:0] spi_data_index;
 
-  logic [DATA_WIDTH-1:0] spi_sclk_counter;
+  logic [DataWidth-1:0] spi_sclk_counter;
 
   logic spi_sclk_prev;
   logic spi_sclk_counter_en;
@@ -170,13 +154,11 @@ module obi_spi #(
 
   // Control Register Value
   assign ctrl_reg_value = (
-      ({5'b0, ctrl_start_writing_bit} << CtrlStartWritingBit)
-    | ({5'b0, ctrl_start_reading_bit} << CtrlStartReadingBit)
-    | ({5'b0, ctrl_busy_bit} << CtrlBusyBit)
-    | ({5'b0, ctrl_tx_buffer_empty_bit} << CtrlTxBufferEmptyBit)
-    | ({5'b0, ctrl_rx_buffer_non_empty_bit} << CtrlRxBufferNonEmptyBit)
-    | ({5'b0, ctrl_complete_bit} << CtrlCompleteBit)
-    | 6'b0
+      ({3'b0, ctrl_start_writing_bit} << CtrlStartWritingBit)
+    | ({3'b0, ctrl_start_reading_bit} << CtrlStartReadingBit)
+    | ({3'b0, ctrl_busy_bit} << CtrlBusyBit)
+    | ({3'b0, ctrl_complete_bit} << CtrlCompleteBit)
+    | 4'b0
   );
 
   /**************************************************************
@@ -185,7 +167,7 @@ module obi_spi #(
 
   // Receive Data Register
   shift_register #(
-    .WORD_WIDTH(SPI_DATA_LENGTH)
+    .WORD_WIDTH(SpiDataLength)
   ) rx_data_reg_inst (
     .clk  (clk_i),
     .rstn (rstn_i),
@@ -199,14 +181,14 @@ module obi_spi #(
     .WORD_WIDTH(4)
   ) spi_data_index_inst (
       .clk (clk_i),
-      .rstn(rstn_i && ~({28'b0, spi_data_index} == SPI_DATA_LENGTH)),
+      .rstn(rstn_i && ~({28'b0, spi_data_index} == SpiDataLength)),
       .ce  (spi_sclk_counter == 0 && ~spi_sclk_o && spi_sclk_prev),
       .count (spi_data_index)
   );
 
   // SPI Serial Clock Counter
   cntr #(
-    .WORD_WIDTH(DATA_WIDTH)
+    .WORD_WIDTH(DataWidth)
   ) spi_sclk_counter_inst (
       .clk (clk_i),
       .rstn(rstn_i && ~(spi_sclk_counter == spi_div_clk_reg) && spi_sclk_counter_en),
@@ -216,10 +198,8 @@ module obi_spi #(
 
   // Previous SPI serial clock
   register spi_sclk_prev_inst (.clk(clk_i), .rstn(rstn_i), .ce('1), .in(spi_sclk_o), .out(spi_sclk_prev));
-  // SPI Serial Clock
 
   assign spi_sclk_ce = (spi_sclk_counter == spi_div_clk_reg) && (spi_div_clk_reg != '0);
-
   register spi_sclk_o_inst (.clk(clk_i), .rstn(rstn_i && spi_ss_o < '1), .ce(spi_sclk_ce), .in(~spi_sclk_o), .out(spi_sclk_o));
 
   /**************************************************************
@@ -231,7 +211,7 @@ module obi_spi #(
   
   assign spi_ss_o = ~(ss_reg);
 
-  assign spi_mosi_o = (spi_state == eSPI_IDLE || spi_data_index == SPI_DATA_LENGTH || spi_state == eSPI_DONE || spi_state == eSPI_READING) ? 1'b0 : tx_data_reg[SPI_DATA_LENGTH - 1 - {28'b0, spi_data_index}];
+  assign spi_mosi_o = (spi_state == eSPI_IDLE || spi_data_index == SpiDataLength || spi_state == eSPI_DONE || spi_state == eSPI_READING) ? 1'b0 : tx_data_reg[SpiDataLength - 1 - {28'b0, spi_data_index}];
   
   assign complete_o = ctrl_complete_bit;
 
@@ -242,8 +222,8 @@ module obi_spi #(
 
   // SPI FSM conditions for transitions
   always_comb begin
-    spi_stopped_writing =   spi_state == eSPI_WRITING  && {28'b0, spi_data_index} == SPI_DATA_LENGTH;
-    spi_stopped_reading = spi_state == eSPI_READING && {28'b0, spi_data_index} == SPI_DATA_LENGTH;
+    spi_stopped_writing =   spi_state == eSPI_WRITING  && {28'b0, spi_data_index} == SpiDataLength;
+    spi_stopped_reading = spi_state == eSPI_READING && {28'b0, spi_data_index} == SpiDataLength;
     spi_completed = spi_state == eSPI_DONE && ~ctrl_complete_bit;
   end
 
@@ -275,18 +255,18 @@ module obi_spi #(
 
   // Transfer Data Register
   register #(
-    .DTYPE(logic[SPI_DATA_LENGTH-1:0])
+    .DTYPE(logic[SpiDataLength-1:0])
   ) tx_data_reg_inst (
     .clk  (clk_i),
     .rstn (rstn_i),
     .ce   (obi_a_write_valid && obi_aaddr_i == TxDataRegAddrOffset && obi_abe_i[0]),
-    .in   (obi_awdata_i[SPI_DATA_LENGTH - 1:0]),
+    .in   (obi_awdata_i[SpiDataLength - 1:0]),
     .out  (tx_data_reg)
   );
 
   // SPI Clock Divisor Register
   register #(
-    .DTYPE(logic[DATA_WIDTH-1:0]),
+    .DTYPE(logic[DataWidth-1:0]),
     .RESET_VALUE(SCLK_COUNTER_RESET_VALUE)
   ) spi_div_clk_reg_inst (
     .clk  (clk_i),
@@ -308,16 +288,16 @@ module obi_spi #(
   );
 
   // OBI Response Data Out Register
-  register #(.DTYPE(logic[DATA_WIDTH-1:0])) obi_rdata_o_inst (.clk(clk_i), .rstn(rstn_i && (obi_a_read || ~obi_done)), .ce(obi_a_read), .in(obi_read_value), .out(obi_rdata_o));
+  register #(.DTYPE(logic[DataWidth-1:0])) obi_rdata_o_inst (.clk(clk_i), .rstn(rstn_i && (obi_a_read || ~obi_done)), .ce(obi_a_read), .in(obi_read_value), .out(obi_rdata_o));
 
   always_comb begin
     unique case (obi_aaddr_i)
-      TxDataRegAddrOffset: obi_read_value = {24'b0, tx_data_reg};
-      RxDataRegAddrOffset: obi_read_value = {24'b0, rx_data_reg};
+      TxDataRegAddrOffset:    obi_read_value = {24'b0, tx_data_reg};
+      RxDataRegAddrOffset:    obi_read_value = {24'b0, rx_data_reg};
       SpiDivClkRegAddrOffset: obi_read_value = spi_div_clk_reg;
-      SsRegAddrOffset: obi_read_value = {28'b0, ss_reg};
-      CtrlRegAddrOffset: obi_read_value = {26'b0, ctrl_reg_value};
-      default:		 obi_read_value = 32'b0; 
+      SsRegAddrOffset:        obi_read_value = {28'b0, ss_reg};
+      CtrlRegAddrOffset:      obi_read_value = {28'b0, ctrl_reg_value};
+      default:		            obi_read_value = 32'b0; 
     endcase
   end
 
@@ -327,19 +307,17 @@ module obi_spi #(
 
   // Output assignment
   assign obi_agnt_o = (obi_state == eOBI_IDLE);
-  assign obi_rvalid_o = (obi_state == eOBI_READING || obi_state == eOBI_WRITING);
+  assign obi_rvalid_o = (obi_state == eOBI_TRANSFER);
 
   // OBI FSM conditions for transitions
   always_comb begin
-    obi_started_reading = obi_state == eOBI_IDLE && obi_a_read;
-    obi_started_writing = obi_state == eOBI_IDLE && obi_a_write;
+    obi_started_transfer = obi_state == eOBI_IDLE && (obi_a_read || obi_a_write);
     obi_done            = obi_rvalid_o && obi_rready_i;
   end
 
   // OBI FSM transitions
   always_comb begin
-    obi_state_next = obi_started_reading  ? eOBI_READING : obi_state;
-    obi_state_next = obi_started_writing  ? eOBI_WRITING : obi_state_next;
+    obi_state_next = obi_started_transfer ? eOBI_TRANSFER : obi_state;
     obi_state_next = obi_done             ? eOBI_IDLE    : obi_state_next;
   end
 
